c950f2092cfea1e75c883a1d5f47c03e13753792
JangUniverse <33-060@hansung-sh.hs.kr>
2025-04-16 22:10:31 +0900

    commit_edited

--- BOJ/2562.cpp
+++ BOJ/2562.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <climits> // INT_MIN을 사용하기 위해 필요
 using namespace std;
 
 int main(int argc, char const *argv[]){
     int arr[9];
-    int maxvalue = INT_MIN; // 가능한 가장 작은 값으로 초기화
+    int maxvalue = 0;
     int maxvalueindex = 0;
     
     for(int i = 0; i < 9; i++){
